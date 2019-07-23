618e619fc20387ca5bb05980e0d7a00f6b6c0bb7
REWRITER <200911263@sangmyung.kr>
2019-07-24 06:14:57 +0900

    rental-account

---
+++ src/main/java/com/skcc/mc/rentalaccount/RentalAccountApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.mc.rentalaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalAccountApplication.class, args);
+    }
+
+}
